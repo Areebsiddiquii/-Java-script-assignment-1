125a3e052b1fb249dff288e6a6866b137d5c8b4d
Areebsiddiquii <153526167+Areebsiddiquii@users.noreply.github.com>
2024-01-29 01:07:54 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,85 @@
+//Q1. Write a script to greet your website visitor using JS alert box. (Welcome to Our Website) 
+
+//alert("Welcome to Our Website");
+
+
+
+
+// Q2. Write a script to display message on your web page. (Error! Please Enter avalid password) 
+
+// alert("Error! Please Enter a valid password");
+
+
+
+
+//Q3. Write a script to display message on your web page: (Hint : Use line break)
+
+// alert("This is the first line.\nThis is the second line.");
+
+
+
+
+//Q4. Write a script to display messages in sequence (3 Alerts)
+
+// alert("Alert 1");
+// alert("Alert 2");
+// alert("Alert 3");
+
+
+
+
+//Q5. Make use of alerts in your new/existing HTML & CSS project.
+
+// alert("This alert is in the head section");
+
+
+
+
+// Q7. Declare a variable called username. 
+
+//var username;
+
+
+
+//Q8. Declare a variable called myName & assign to it a string that represents your Full Name.  
+
+//var myName = "Areeb Siddiqui";
+
+
+
+// Q9. Write script to  
+// Qa) Declare a JS variable, titled message.  
+// Qb) Assign “Hello World” to variable message  
+// Qc) Display the message in alert box. 
+
+// a)
+//var message;
+
+// b)
+//message = "Hello World";
+
+// c)
+//alert(message);
+
+
+
+
+//Q10. Write a script to save student’s bio data in JS variables and show the data in alert boxes. 
+
+
+//var studentName = "Areeb Siddiqui";
+// var age = 20;
+// var grade = "A";
+// var address= " liaquatabad No 6"; 
+
+// alert("Student Name: " + studentName + "\nAge: " + age + "\nGrade: " + grade + "\nAddress:"+ address);
+
+
+
+// Q11. Declare a variable called email and assign to it a string that represents your 
+// Email Address(e.g. example@example.com). Show the blow mentioned 
+// message in an alert box.(Hint: use string concatenation) 
+
+//var email = "areeb.rahid12123@gmail.com";
+//alert("My email address is: " + email);
+
